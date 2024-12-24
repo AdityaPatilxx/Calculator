baad0efbeac7ba9e5b8b4eab288abffd6fba0171
AdityaPatilxx <161197993+AdityaPatilxx@users.noreply.github.com>
2024-12-24 15:44:54 +0530

    feat: refactor calculator display and update operation logic

--- script.js
+++ script.js
@@ -1,44 +1,80 @@
-function addition(firstNumber, secondNumber) {
-    return firstNumber + secondNumber;
-}
 
-function subtraction(firstNumber, secondNumber) {
-    return firstNumber - secondNumber;
-}
+function operate(string) {
 
-function division(firstNumber, secondNumber) {
-    return firstNumber / secondNumber;
-}
+    let numberArray = string.match(/(\d+(\.\d+)?)/g);
+    let operatorArray = string.match(/[+\-×÷]/g);
 
-function multiplication(firstNumber, secondNumber) {
-    return firstNumber * secondNumber;
-}
 
-function operate(firstNumber, operator, secondNumber) {
+
+    let firstNumber = numberArray[0]
+    let secondNumber = numberArray[1]
+
+    if (numberArray.length < 2 || operatorArray.length < 1) {
+        return firstNumber
+    }
+
+    let operator = operatorArray[0]
+
     let answer = 0
     switch (operator) {
         case '+':
-            return addition(firstNumber, secondNumber);
+            return firstNumber + secondNumber
 
         case '-':
-            return subtraction(firstNumber, secondNumber);
+            return firstNumber - secondNumber
 
-        case '*':
-            return multiplication(firstNumber, secondNumber);
+        case '×':
+            return firstNumber * secondNumber
 
-        case '/':
-            return division(firstNumber, secondNumber);
+        case '÷':
+            return firstNumber / secondNumber
+        default:
+            return 'error'
+
     }
 }
 
+function splitExpression(string) {
+    let splitArray = string.match(/(\d+(\.\d+)?|[+\-*/])/g);
+    return splitArray;
+}
+
+const expression = document.querySelector('#expression')
+const answer = document.querySelector('#answer')
 function display() {
     const buttonContainer = document.querySelector('.button-container')
-    const display = document.querySelector('#real-display')
 
     buttonContainer.addEventListener('click', (event) => {
         let target = event.target
-        display.textContent = target.textContent
+        if (target.className == 'numbers' || target.className == 'operator') {
+            expression.textContent = expression.textContent + target.textContent
+            answer.textContent = operate(expression.textContent)
+
+        }
+        else if (target.id == 'erase') {
+            erase('one')
+            answer.textContent = operate(expression.textContent)
+        }
+        else if (target.id == 'erase-all') {
+            erase('all')
+            answer.textContent = operate(expression.textContent)
+        }
+        else if (target.id == 'start') {
+            expression.textContent = operate(expression.textContent)
+        }
     })
 }
 
 display()
+console.log(expression.textContent)
+
+
+function erase(flag) {
+    if (flag == 'all') {
+        expression.textContent = ''
+        answer.textContent = ''
+    }
+    else if (flag == 'one') {
+        expression.textContent = expression.textContent.slice(0, -1);
+    }
+}
