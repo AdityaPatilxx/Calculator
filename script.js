416a267c45e2e57e22e63fb67df7b8dec5282344
AdityaPatilxx <161197993+AdityaPatilxx@users.noreply.github.com>
2024-12-24 21:40:35 +0530

    feat: initialize calculator display with default value and enhance operation handling

--- script.js
+++ script.js
@@ -1,21 +1,20 @@
+const operatorPattern = /[+\-×÷^]/g;
+
 
 function operate(string) {
 
     let numberArray = string.match(/(\d+(\.\d+)?)/g);
-    let operatorArray = string.match(/[+\-×÷]/g);
-
-
-
-    let firstNumber = numberArray[0]
-    let secondNumber = numberArray[1]
+    let operatorArray = string.match(operatorPattern);
 
     if (numberArray.length < 2 || operatorArray.length < 1) {
-        return firstNumber
+        return numberArray[0]
     }
 
-    let operator = operatorArray[0]
+    return mathsOperation(numberArray[0], numberArray[1], operatorArray[0])
+}
 
-    let answer = 0
+function mathsOperation(firstNumber, secondNumber, operator) {
+
     switch (operator) {
         case '+':
             return firstNumber + secondNumber
@@ -28,17 +27,19 @@ function operate(string) {
 
         case '÷':
             return firstNumber / secondNumber
+
+        case '^':
+            return Math.pow(firstNumber, secondNumber)
+
         default:
             return 'error'
 
     }
 }
 
-function splitExpression(string) {
-    let splitArray = string.match(/(\d+(\.\d+)?|[+\-*/])/g);
-    return splitArray;
-}
 
+
+let operatorCounter = 1
 const expression = document.querySelector('#expression')
 const answer = document.querySelector('#answer')
 function display() {
@@ -46,10 +47,17 @@ function display() {
 
     buttonContainer.addEventListener('click', (event) => {
         let target = event.target
-        if (target.className == 'numbers' || target.className == 'operator') {
+        if (target.className == 'numbers') {
+            (expression.textContent == '0')
+                ? expression.textContent = target.textContent
+                : expression.textContent = expression.textContent + target.textContent
+
+            answer.textContent = operate(expression.textContent)
+        }
+        else if (target.className == 'operator' && operatorCounter < 2) {
             expression.textContent = expression.textContent + target.textContent
             answer.textContent = operate(expression.textContent)
-
+            operatorCounter++
         }
         else if (target.id == 'erase') {
             erase('one')
@@ -58,23 +66,28 @@ function display() {
         else if (target.id == 'erase-all') {
             erase('all')
             answer.textContent = operate(expression.textContent)
+            operatorCounter = 1
         }
         else if (target.id == 'start') {
             expression.textContent = operate(expression.textContent)
+            operatorCounter = 1
         }
     })
 }
 
-display()
-console.log(expression.textContent)
-
-
 function erase(flag) {
     if (flag == 'all') {
-        expression.textContent = ''
+        expression.textContent = '0'
         answer.textContent = ''
     }
     else if (flag == 'one') {
+        let lastErasedCharacter = expression.textContent.at(-1)
+        console.log(lastErasedCharacter)
+        if (operatorPattern.test(lastErasedCharacter)) {
+            operatorCounter = 1
+        }
         expression.textContent = expression.textContent.slice(0, -1);
     }
 }
+
+display()
